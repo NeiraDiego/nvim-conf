04408d964c5a558c5b2f63d44690ac6fe5c94231
Diego <diegoneira@gmail.com>
2024-12-17 16:44:10 -0300

    lualine

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -15,7 +15,6 @@ return {
     indent = { enable = true },
     auto_install = true, -- automatically install syntax support when entering new file type buffer
     ensure_installed = {
-      'lua',
     },
   },
   config = function (_, opts)
